741b1613b322df1a6c3d3afeb3013e4646712c40
alexFrankfurt <alex_frankfurt@mail.ru>
2017-11-09 21:35:45 +0300

    add better files

--- build.sbt
+++ build.sbt
@@ -1,1 +1,3 @@
-scalaVersion := "2.12.3"
+scalaVersion := "2.12.4"
+
+libraryDependencies += "com.github.pathikrit" %% "better-files" % "3.2.0"
--- src/main/scala/com/alex/Main.scala
+++ src/main/scala/com/alex/Main.scala
@@ -1,9 +1,15 @@
 package com.alex
 
+import better.files._
+
 object Main {
   def main(args: Array[String]): Unit = {
+    val f: File = "tmp.txt".toFile
+    f.createIfNotExists()
 
-    val arr =Array(Point(3, 4), Point(2, 8), Point(1, 10))
+
+
+    val arr = Array(Point(3, 4), Point(2, 8), Point(1, 10))
     util.Sorting.quickSort(arr)(PointOrdering)
     println(arr.mkString(", "))
   }
